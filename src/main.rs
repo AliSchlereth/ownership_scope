c623ef5ae4d73c0339ec947fb9d616e42c66751d
Ali Schlereth <aschlere@sbcglobal.net>
2017-08-09 15:25:32 -0600

    Add return value of function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 fn main() {
-    let s = String::from("hello");
-
+    let s = String::from("hello s");
     takes_ownership(s);
     // println!("Still here? {}", s);
+
+    let s1 = gives_ownership();
+
+    println!("Still Here? {}", s1);
+
+    let s2 = String::from("hello s2");
+
+
+    let s3 = takes_and_gives_back(s2);
+
+    // println!("Still Here? {}", s2);
+    println!("Still Here? {}", s3);
 
     let x = 5;
 
@@ -17,4 +28,14 @@ fn takes_ownership(some_string: String) {
 
 fn makes_copy(some_integer: i32) {
     println!("{}", some_integer);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello giver");
+
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
